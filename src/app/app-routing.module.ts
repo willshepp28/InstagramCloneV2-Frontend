3ad742c35e75ebe94bff6b39145bcd53c744e844
Will Sheppard <28759252+willshepp28@users.noreply.github.com>
2019-11-12 18:55:14 -0500

    setup inital folder structure; created login and signup routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './routes/login/login.component';
+import { SignupComponent } from './routes/signup/signup.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent, },
+  { path: 'signup', component: SignupComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
