5e1fe96c64b8b3bfb78c2ddb446318b420b3d46c
Will Sheppard <28759252+willshepp28@users.noreply.github.com>
2019-11-13 20:14:52 -0500

    removed login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './routes/accounts/login/login.component';
 import { SignupComponent } from './routes/accounts/signup/signup.component';
 import { IgFormComponent } from './routes/accounts/ig-form/ig-form.component';
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './routes/accounts/login/login.component';
 import { SignupComponent } from './routes/accounts/signup/signup.component';
 import { SignupInputsComponent } from './routes/accounts/signup-inputs/signup-inputs.component';
 import { LoginInputsComponent } from './routes/accounts/login-inputs/login-inputs.component';
@@ -12,7 +11,6 @@ import { IgFormComponent } from './routes/accounts/ig-form/ig-form.component';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     SignupComponent,
     SignupInputsComponent,
     LoginInputsComponent,
