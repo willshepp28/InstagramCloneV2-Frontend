2d075027320bb458467015b5a2ab7ff1a988c4c9
Will Sheppard <28759252+willshepp28@users.noreply.github.com>
2019-11-13 22:19:37 -0500

    setup explore instagram-feed and profile-tab routes in app-router module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { IgFormComponent } from './routes/accounts/ig-form/ig-form.component';
+import { ExploreComponent } from './routes/explore/explore.component';
+import { InstagramFeedComponent } from './routes/instagram-feed/instagram-feed.component';
+import { ProfileTabComponent } from './routes/profile-tab/profile-tab.component';
 
 
 const routes: Routes = [
   { path: 'accounts/login', component: IgFormComponent, },
   { path: 'accounts/emailsignup', component: IgFormComponent },
+  { path: '', component: InstagramFeedComponent},
+  { path: ':username' , component: ProfileTabComponent},
+  { path: 'explore', component: ExploreComponent},
   { path: '', redirectTo: '/accounts/emailsignup', pathMatch: 'full' }
 ];
 
