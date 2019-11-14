1c8c36e70eb60317f3309c736f8c057edc3084d6
Will Sheppard <28759252+willshepp28@users.noreply.github.com>
2019-11-13 22:10:54 -0500

    Revert ","
    This reverts commit 6cbc351952070280ce9e951257b795e266c2bc98.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,13 @@ import { AppComponent } from './app.component';
 import { SignupInputsComponent } from './routes/accounts/signup-inputs/signup-inputs.component';
 import { LoginInputsComponent } from './routes/accounts/login-inputs/login-inputs.component';
 import { IgFormComponent } from './routes/accounts/ig-form/ig-form.component';
-import { ExploreComponent } from './routes/explore/explore.component';
-import { InstagramFeedComponent } from './routes/instagram-feed/instagram-feed.component';
-import { ProfileTabComponent } from './routes/profile-tab/profile-tab.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupInputsComponent,
     LoginInputsComponent,
-    IgFormComponent,
-    ExploreComponent,
-    InstagramFeedComponent,
-    ProfileTabComponent
+    IgFormComponent
   ],
   imports: [
     BrowserModule,
